9be82d2b41a27e5067d6dd8e06a8d2cff2defe23
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-13 01:47:22 +0000

    updated fether.js

--- fether.js
+++ fether.js
@@ -7,17 +7,20 @@ const path = process.argv[3]
 const fetcher = (url, path) => {
   request(url , (error, response, body) => {
     fs.writeFile(path, body, function(err) {
-      if (err) throw err;
-        console.log(`Downloaded and saved ${getFilesizeInBytes(path)} bytes to ${path}`)
+      fs.stat(path, (err, stat) => {
+        const size = stat.size
+        if (err) throw err;
+          console.log(`Downloaded and saved ${size} bytes to ${path}`)
+        })
       })
     })
 };
 
-const getFilesizeInBytes = (filename) => {
-  const stats = fs.statSync(filename)
-  const fileSizeInBytes = stats["size"]
-  return fileSizeInBytes;
-}
+// const getFilesizeInBytes = (filename) => {
+//   const stats = fs.statSync(filename)
+//   const fileSizeInBytes = stats["size"]
+//   return fileSizeInBytes;
+// }
 
 fetcher(url, path)
 
