a168c68e66225e1db2214b48d24bd977d3d26e59
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-13 00:43:22 +0000

    adding fether.js

--- fether.js
+++ fether.js
@@ -3,15 +3,22 @@ const fs = require('fs')
 const url = process.argv[2]
 const path = process.argv[3]
 
+
 const fetcher = (url, path) => {
-  request(url , (body) => {
+  request(url , (error, response, body) => {
     fs.writeFile(path, body, function(err) {
       if (err) throw err;
-        console.log(`Saved to ${path}`)
+        console.log(`Downloaded and saved ${getFilesizeInBytes(path)} bytes to ${path}`)
       })
     })
 };
 
+const getFilesizeInBytes = (filename) => {
+  const stats = fs.statSync(filename)
+  const fileSizeInBytes = stats["size"]
+  return fileSizeInBytes;
+}
+
 fetcher(url, path)
 
-// node fetcher.js https://www.google.com/ ./index.html
+// node fether.js https://www.google.com/ ./index.html
